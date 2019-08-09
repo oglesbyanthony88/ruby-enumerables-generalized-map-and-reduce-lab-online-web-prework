9130a0c80a5beb95c6686928295d1930e350116c
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-09 17:45:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(array)
+  new_array = []
+  counter = 0
+  while counter < array.length do
+    new_array.push(yield(array[counter]))
+    counter += 1
+  end
+  new_array
+end
+
+def reduce(array, value = nil)
+  if value
+    reduced = value
+    i = log10
+  else
+    reduced = array[0]
+    i = 1
+  end
+  while i < array.length
+    reduced = yeild(reduced, array[i])
+    i += 1
+  end
+  return reduced
+end
