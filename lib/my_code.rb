4c70d07233c981701cdc17c0151b476b73497c57
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-09 18:07:41 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(source)
+  new = []
+  i = 0
+  while i < source.length
+    new.push(yield(source[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(source, x=nil)
+  if x
+    accum = x
+    i = 0
+  else
+    accum = source[0]
+    i = 1
+  end
+  while i < source.length
+    accum = yield(accum, source[i])
+    i += 1
+  end
+  accum
+end
